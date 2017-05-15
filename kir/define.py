5dd1cd031669ff54689a1463a4acb4b7b79770c7
Toshiaki Tanaka <Toshiaki.Tanaka@jp.sony.com>
2017-05-16 07:09:26 +0900

    update.

---
+++ kir/define.py
@@ -0,0 +1,3 @@
+import os
+
+KIR_ROOT = os.path.normpath(os.path.dirname(__file__))
---
+++ test/test_cmd.py
@@ -0,0 +1,45 @@
+from nose.tools import with_setup, raises, ok_, eq_, timed
+from kir import cmd
+from kir.exception import *
+
+def setup():
+    pass
+
+def teardown():
+    pass
+
+@with_setup(setup, teardown)
+def test_cmd_run_success_01():
+    result = cmd.run("ls")
+    eq_(result[0], 0)
+
+@with_setup(setup, teardown)
+def test_cmd_run_failed_01():
+    result = cmd.run('ls -la | wc -l')
+    eq_(result[0], 2)
+
+@with_setup(setup, teardown)
+@raises(RunError)
+def test_cmd_run_failed_02():
+    result = cmd.run("hoge")
+    eq_(result[0], 1)
+
+@with_setup(setup, teardown)
+@timed(2.2)
+def test_cmd_run_sleep_success():
+    result = cmd.run("sleep 2", timeout=5)
+    eq_(result[0], 0)
+
+@with_setup(setup, teardown)
+@raises(TimeoutError)
+def test_cmd_run_sleep_timeout_01():
+    result = cmd.run("sleep 10", timeout=5)
+    eq_(result[0], 0)
+
+@with_setup(setup, teardown)
+@timed(5.2)
+def test_cmd_run_sleep_timeout_02():
+    try:
+        result = cmd.run("sleep 10", timeout=5)
+    except TimeoutError as e:
+        ok_(True)
